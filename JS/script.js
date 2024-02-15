0e2daf16358bea81cb3f9e995de4180172e1bb3e
Anunay Argha <anunayargha@gmail.com>
2024-02-15 23:23:03 +0600

    project rebuilding

--- JS/script.js
+++ JS/script.js
@@ -17,11 +17,20 @@ const generateAlphabet = function () {
 	return alphabets[randomInt].toUpperCase();
 };
 
+const showSection = function (section) {
+	document.getElementById(section).classList.remove("hidden");
+};
+
+const hideSection = function (section) {
+	document.getElementById(section).classList.add("hidden");
+};
+
 // * EVENT HANDLERS -
 
 document.querySelectorAll(".btn").forEach((btn) => {
 	btn.addEventListener("click", function (e) {
-		document.getElementById("play-screen").classList.remove("hidden");
+		alphabet.textContent = generateAlphabet();
+		showSection("play-screen");
 		e.target.closest("section").classList.add("hidden");
 	});
 });
@@ -36,12 +45,12 @@ document.addEventListener("keyup", function (e) {
 		}
 	});
 
-	alphabet.textContent = generateAlphabet();
 	if (life > 0) {
 		e.key.toUpperCase() === alphabet.textContent ? score++ : life--;
 		gameScore.textContent = score;
 		gameLife.textContent = life;
 	} else {
+		document.getElementById("gameScore").textContent = score;
 		document.querySelectorAll("section").forEach((section) => {
 			if (section.getAttribute("id") === "score-screen") {
 				section.classList.remove("hidden");
@@ -49,6 +58,12 @@ document.addEventListener("keyup", function (e) {
 				section.classList.add("hidden");
 			}
 		});
-		document.getElementById("gameScore").textContent = score;
+	}
+	alphabet.textContent = generateAlphabet();
+	if (e.key === "Escape") {
+		document.querySelectorAll("section").forEach((sec) => {
+			hideSection(sec.getAttribute("id"));
+		});
+		showSection("home-screen");
 	}
 });
